5b8c0b9be2af84d81bd8cad71d7df8e1186b73d7
Steveplays28 <djspaargaren@outlook.com>
2022-01-25 15:06:21 +0100

    Laser gun

--- scripts/LaserWeapon.cs
+++ scripts/LaserWeapon.cs
@@ -4,5 +4,35 @@ using Godot;
 public class LaserWeapon : Node2D
 {
 	[Export] public float damage = 10f;
-	[Export] public float shootSpeed = 1f;
+	[Export] public float shootSpeed = 0.25f;
+	[Export] public float bulletSpeed = 1f;
+	[Export] public string bulletResourcePath = "res://prefabs/laser_bullet.tscn";
+
+	private float lastShotTime = 0f;
+
+	public override void _Ready()
+	{
+		lastShotTime = OS.GetTicksMsec() / 1000;
+	}
+
+	public override void _Process(float delta)
+	{
+		if (Input.IsActionPressed("shoot") && OS.GetTicksMsec() / 1000 - lastShotTime >= shootSpeed)
+		{
+			Shoot();
+			lastShotTime = OS.GetTicksMsec() / 1000;
+		}
+
+		// GD.Print($"lastShotTime: {lastShotTime} ----- OS.GetTicksMsec() / 1000 - lastShotTime: {OS.GetTicksMsec() / 1000 - lastShotTime}");
+	}
+
+	public void Shoot()
+	{
+		RigidBody2D bullet = GD.Load<PackedScene>(bulletResourcePath).Instance<RigidBody2D>();
+		GetNode("../../").AddChild(bullet);
+
+		bullet.Position = GlobalPosition;
+		bullet.Rotation = GlobalRotation;
+		bullet.LinearVelocity = new Vector2(0, -bulletSpeed).Rotated(bullet.Rotation);
+	}
 }
--- scripts/Player.cs
+++ scripts/Player.cs
@@ -14,7 +14,7 @@ public class Player : RigidBody2D
 	public Camera2D camera;
 
 	private Vector2 direction = Vector2.Zero;
-	private string state = "stopped";
+	private string playerState = "stopped";
 
 	public override void _Ready()
 	{
@@ -32,9 +32,15 @@ public class Player : RigidBody2D
 	public override void _IntegrateForces(Physics2DDirectBodyState state)
 	{
 		if (Input.IsActionPressed("move_forward"))
+		{
 			AppliedForce = _thrust.Rotated(Rotation);
+			playerState = "moving forward";
+		}
 		else
+		{
 			AppliedForce = new Vector2();
+			playerState = "slowing down";
+		}
 
 		var rotationDir = 0;
 		if (Input.IsActionPressed("move_right"))
@@ -45,12 +51,14 @@ public class Player : RigidBody2D
 
 		// TODO: get mouse rotation working without borking the physics
 		// AngularVelocity = (GetGlobalMousePosition() - Position).Angle() + (90 * Mathf.Pi / 180);
-		Transform2D xform = state.Transform.Rotated((GetGlobalMousePosition() - Position).Angle());
-		state.Transform = xform;
+		// Transform2D xform = state.Transform.Rotated((GetGlobalMousePosition() - Position).Angle());
+		// state.Transform = xform;
+
+		// TODO: max velocity
 	}
 
 	public override void _PhysicsProcess(float delta)
 	{
-		GetNode<Label>("/root/Node2D/UI/Debug info").Text = $"Delta time: {delta}\n\nPosition: {Position}\nVelocity: {LinearVelocity}\nState: {state}";
+		GetNode<Label>("/root/Node2D/UI/Debug info").Text = $"Delta time: {delta}\n\nPosition: {Position}\nVelocity: {LinearVelocity}\nState: {playerState}";
 	}
 }
