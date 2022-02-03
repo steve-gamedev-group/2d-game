2881617a17652e60ac2047a68a6531ccb9ddf954
Steveplays28 <djspaargaren@outlook.com>
2022-02-03 16:59:17 +0100

    Dash (wip)

--- scripts/upgrades/Dash.cs
+++ scripts/upgrades/Dash.cs
@@ -1,24 +1,65 @@
 using System;
 using Godot;
+using SteveExtensionMethods;
 
-public class Dash : Node2D
+public class Dash : Node
 {
-	[Export] public float speed = 25f;
-	[Export] public float range = 50f;
+	[Export] public Curve speedCurve = new Curve();
+
+	public bool isDashing { get; private set; } = false;
 
 	private Node localPlayer;
 	private RigidBody2D localPlayerRb2D;
 
+	private int currentDashFrame = 0;
+
 	public override void _Ready()
 	{
-		localPlayerRb2D = GetNode<RigidBody2D>("/root/Node2D/Player/RigidBody2D");
+		localPlayer = GetNode<Node>("/root/Node2D/Player");
+		localPlayerRb2D = localPlayer.GetNode<RigidBody2D>("./RigidBody2D");
+
+		// speedCurve.AddPoint(new Vector2(0f, 0f), 0f, 0.5f);
+		// speedCurve.AddPoint(new Vector2(1f, 1000f), 0.5f, 0f);
+		speedCurve.BakeResolution = Engine.IterationsPerSecond;
+		speedCurve.Bake();
 	}
 
 	public override void _PhysicsProcess(float delta)
 	{
 		if (Input.IsActionJustPressed("dash"))
 		{
-			localPlayerRb2D.GlobalPosition = localPlayerRb2D.GlobalPosition.Normalized().Slerp(new Vector2(0f, range).Rotated(Rotation), 0.5f);
+			StartDash();
 		}
+
+		if (isDashing)
+		{
+			DashProcess();
+		}
+	}
+
+	public void StartDash()
+	{
+		isDashing = true;
+		currentDashFrame = 0;
+	}
+
+	public void StopDash()
+	{
+		isDashing = false;
+	}
+
+	private void DashProcess()
+	{
+		if (currentDashFrame >= speedCurve.BakeResolution)
+		{
+			StopDash();
+			return;
+		}
+
+		GD.Print(speedCurve.GetPointCount());
+
+		localPlayerRb2D.AppliedForce += new Vector2(0f, speedCurve.GetPointPosition(currentDashFrame).y * 1000f).
+			Rotated(localPlayerRb2D.Rotation + 180f.DegToRad());
+		currentDashFrame += 1;
 	}
 }
